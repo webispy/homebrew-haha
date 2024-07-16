50f3b4b59a76da10b23dfbe4489853da1e8f4a23
Inho Oh <webispy@gmail.com>
2024-07-16 08:53:46 +0000

    aln: add 0.1.0 bottle.

--- Formula/aln.rb
+++ Formula/aln.rb
@@ -7,6 +7,13 @@ class Aln < Formula
 
   head "https://github.com/webispy/aln.git", branch: "master"
 
+  bottle do
+    root_url "https://github.com/webispy/homebrew-haha/releases/download/aln-0.1.0"
+    sha256 cellar: :any,                 arm64_sonoma: "1521a642335a82a6e039109f22730476d7180e92a1fe6ffb04348e01a42503d9"
+    sha256 cellar: :any,                 ventura:      "8143c23b5fb4650b31dc6ca90eaaa6c3ea15615ff405fed4a1c588bc3e6ecba9"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "1eb42a074a8698120b27d65d460dc2e1feb7a959a0edb3c20dd3190eeddb0221"
+  end
+
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
   depends_on "glib"
